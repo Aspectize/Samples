12446e4338711a11147b3a6d184f81338b6b62eb
Nicolas Roux <nicolas.roux@aspectize.com>
2014-12-04 17:00:27 +0100

    Delete Connection String

--- SamplesAndDemo/Configuration/Services/ConfiguredServices.js
+++ SamplesAndDemo/Configuration/Services/ConfiguredServices.js
@@ -1,7 +1,7 @@
 ﻿var ADWDB = Aspectize.ConfigureNewService("ADWDB", "DataBaseService");
 
 ADWDB.DataBaseType = aas.ConfigurableServices.DataBaseService.DBMS.AzureStorage;
-ADWDB.ConnectionString = "AccountName=adventureworks;AccountKey=bpF8EHuzmIl+WEdFox77OIhC+QrEDhFSnHt2CHSyXHYgk5aTkgpkkzv9Q2EaBR1JNXlfHuae4HeUOcD1V4yJBw==;DefaultEndpointsProtocol=https;StorageStrategy=NewSort;";
+ADWDB.ConnectionString = "";
 ADWDB.Trace = false;
 ADWDB.EnsureAuthenticationOnWrite = true;
 ADWDB.BuildNewTableOnSave = false;
@@ -9,7 +9,7 @@ ADWDB.BuildNewTableOnSave = false;
 var ADWFileService = Aspectize.ConfigureNewService("ADWFileService", "FileService");
 
 ADWFileService.StorageType = aas.ConfigurableServices.FileService.Storage.Azure;
-ADWFileService.ConnectionString = "AccountName=adventureworks;AccountKey=bpF8EHuzmIl+WEdFox77OIhC+QrEDhFSnHt2CHSyXHYgk5aTkgpkkzv9Q2EaBR1JNXlfHuae4HeUOcD1V4yJBw==;DefaultEndpointsProtocol=https;StorageStrategy=Standard;";
+ADWFileService.ConnectionString = "";
 ADWFileService.RootDirectory = "~\Demo";
 ADWFileService.Trace = false;
 ADWFileService.AccessMode = aas.ConfigurableServices.FileService.FileAccessMode.Private;
