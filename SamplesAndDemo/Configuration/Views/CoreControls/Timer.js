6eb910b17094d4e1327d421ae54ba675adff20eb
nico <nicolas.roux@aspectize.com>
2017-05-24 17:01:55 +0200

    Patch Timer

--- SamplesAndDemo/Configuration/Views/CoreControls/Timer.js
+++ SamplesAndDemo/Configuration/Views/CoreControls/Timer.js
@@ -1,9 +1,8 @@
 
 var vTimer = Aspectize.CreateView("Timer", aas.Controls.Timer, aas.Zones.SideBarContent.ZoneContent);
-vTimer.ActivateTimer.click.BindCommand(aas.Services.Browser.UIService.ActivateTimer, {controlName: vTimer.Timer});
+vTimer.ActivateTimer.click.BindCommand(aas.Services.Browser.UIService.ActivateTimer(aas.ViewName.Timer.Timer, vTimer.TxtCount.value));
 vTimer.BtnResetTrace.click.BindCommand(aas.Services.Browser.DataService.ClearData(aas.Path.MainData.TimerTrace));
-vTimer.DeactivateTimer.click.BindCommand(aas.Services.Browser.UIService.DeactivateTimer, {controlName: vTimer.Timer});
-vTimer.Timer.Count.BindData(vTimer.TxtCount.value);
+vTimer.DeactivateTimer.click.BindCommand(aas.Services.Browser.UIService.DeactivateTimer(aas.ViewName.Timer.Timer));
 vTimer.Timer.Period.BindData(vTimer.TxtPeriod.value);
 vTimer.Timer.OnTimer.BindCommand(aas.Services.Browser.DataService.AddRowAndSelect, {schemaPath: aas.Path.MainData.TimerTrace});
 vTimer.OnActivated.BindCommand(aas.Services.Browser.BootStrapClientService.ActiveLiElement, {element: aas.ViewName.Timer});
